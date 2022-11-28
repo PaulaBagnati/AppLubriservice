5418da120f6d0cfa69f67027380f7e942843e987
Paula Bagnati <pau.bagnati@gmail.com>
2022-11-28 19:58:58 -0300

    cotizador finalizado

--- script.js
+++ script.js
@@ -1,36 +1,72 @@
 alert ("Bienvenidos a DINERO FACIL. ¡Cotiza tu préstamo YA!");
-let monto = Number(prompt("¿Cuánto necesitas? de $10.000 a $100.000"));
-let cuotas = Number(prompt("¿A qué plazo quieres pagarlo? : 6 - 12 - 18"));
 
+let monto = 0;
 
+while(monto == 0){
 
+    monto = Number(prompt("¿Cuánto necesitas? de $10000 a $100000"));
+    
+    if (montoCorrecto(monto)){
+    
+        let cuotas = 0;
+        
+        while(cuotas== 0){
 
-    function calcularCuotas(monto, cuotas){
-        if(monto >= 10000 && monto <= 100000) {
+            cuotas = Number(prompt("¿A qué plazo quieres pagarlo? : 6 - 12 - 18"));
 
-            switch(cuotas){
-                case 6:
-                    resultado = monto / 6 
-                    alert (`Pagaras 6 cuotas de ${resultado.toFixed(2)}`);
-                    break;
-
-                case 12:
-                resultado = monto / 12 
-                    alert (`Pagaras 12 cuotas de ${resultado.toFixed(2)}`);
-                    break;
-                    
-
-                case 18:
-                    resultado =monto / 18 
-                    alert (`Pagaras 18 cuotas de ${resultado.toFixed(2)}`);
-                    break;
-                    
-
-                default:
-                    alert (`No seleccionaste cuotas`);
-            }        
-        } 
+            if(cuotas != 6 && cuotas != 12 && cuotas != 18){
+                alert("Por favor, ingrese una cantidad admitida")  
+                cuotas = 0;
+            } else {
+                calcularCuotas(monto, cuotas);
+            }
+        }
+    
+        
+    } else {
+        alert("El monto debe estar entre $10000 y $100000");    
+        monto = 0;        
     }
 
+}
+
+function montoCorrecto(monto){
+
+    if(monto >= 10000 && monto <= 100000){
+        return true;
+    }else{
+        return false;
+    }    
+
+}
+
+function calcularCuotas(monto, cuotas){
+
+    let valorMensual = monto / cuotas;
+    let porcentaje = (valorMensual * 100) / 5;
+    let valorTotal = valorMensual + porcentaje;
+
     
-    calcularCuotas(monto, cuotas);
+	resultado = valorTotal / cuotas;
+
+	switch(cuotas){
+        case 6:
+            alert (`Pagaras 6 cuotas de ${resultado.toFixed(2)}`);
+		break;
+
+        case 12:
+        
+            alert (`Pagaras 12 cuotas de ${resultado.toFixed(2)}`);
+        break;
+                    
+
+        case 18:
+            alert (`Pagaras 18 cuotas de ${resultado.toFixed(2)}`);
+        break;
+
+
+        default:
+            alert (`Ingresar un número válido de cuotas : 6 - 12 - 18`);
+    }  
+
+}
